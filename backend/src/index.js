7140a8b2416dd6475d69812e18abc84a4702a93e
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-02 11:25:40 -0300

    [ADD] #start - Adicionada estrutura de start do backend do projeto

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    "mongodb://localhost:27017/estoque",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+app.listen(3001)
---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+class Routes {
+    constructor() {
+        this.routes = new Router();
+
+        // Load routes here
+    }
+}
